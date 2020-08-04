c97bd39944093e35c483d04ec1dfba511533a869
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-08-04 17:27:36 +0200

    Added minimum points for OIPS

--- src/oips.jl
+++ src/oips.jl
@@ -1,6 +1,6 @@
 """
-    OIPS(ρ_accept=0.8, opt= ADAM(0.001); η = 0.95, kmax = Inf, ρ_remove = Inf )
-    OIPS(kmax, η, opt= ADAM(0.001))
+    OIPS(ρ_accept=0.8, opt= ADAM(0.001); η = 0.95, kmax = Inf, kmin = 10, ρ_remove = Inf )
+    OIPS(kmax, η = 0.98, kmin = 10)
 
 Online Inducing Points Selection.
 Method from the paper include reference here.
@@ -9,6 +9,7 @@ mutable struct OIPS{S,TZ<:AbstractVector{S}} <: OnIP{S,TZ}
     ρ_accept::Float64
     ρ_remove::Float64
     kmax::Float64
+    kmin::Float64
     η::Float64
     k::Int
     Z::TZ
@@ -22,8 +23,9 @@ Base.show(io::IO, alg::OIPS) = print(
 function OIPS(
     ρ_accept::Real = 0.8;
     η::Real = 0.95,
-    kmax = Inf,
+    kmax::Real = Inf,
     ρ_remove::Real = Inf,
+    kmin::Real = 10,
 )
     @assert 0.0 <= ρ_accept <= 1.0 "ρ_accept should be between 0 and 1"
     @assert 0.0 <= η <= 1.0 "η should be between 0 and 1"
@@ -33,77 +35,78 @@ function OIPS(
         ρ_accept,
         ρ_remove,
         kmax,
+        kmin,
         η,
         0,
         [],
     )
 end
 
-function OIPS(kmax::Int, η::Real = 0.98)
+function OIPS(kmax::Int, η::Real = 0.98, kmin::Real = 10)
     @assert kmax > 0 "kmax should be bigger than 0"
     @assert 0.0 <= η <= 1.0 "η should be between 0 and 1"
     return OIPS(
         0.95,
         sqrt(0.95),
         kmax,
+        kmin,
         η,
         0,
         [],
     )
 end
 
-function OIPS(alg::OIPS, X::AbstractVector)
+function OIPS(Z::OIPS, X::AbstractVector, k::Kernel)
     N = size(X, 1)
-    N >= 10 || "First batch should have at least 10 samples"
+    N >= Z.kmin || "First batch should have at least $(Z.kmin) samples"
     samples = sample(1:N, 10, replace = false)
-    return OIPS(alg.ρ_accept, alg.ρ_remove, alg.kmax, alg.η, 10, Z)
+    return OIPS(Z.ρ_accept, Z.ρ_remove, Z.kmax, Z.kmin, Z.η, 10, deepcopy(X[samples]))
 end
 
-function init(alg::OIPS, X::AbstractVector, gp)
+function init(alg::OIPS, X::AbstractVector, k::Kernel)
     alg = OIPS(alg, X)
     update!(alg, X, gp)
     return alg
 end
 
-function update!(alg::OIPS, X::AbstractVector, gp)
-    add_point!(alg, X, gp)
-
+function update!(Z::OIPS, X::AbstractVector, k::Kernel)
+    add_point!(Z, X, k)
 end
 
-function add_point!(alg::OIPS, X::AbstractVector, gp)
+function add_point!(Z::OIPS, X::AbstractVector, k::Kernel)
     b = size(X, 1)
     for i = 1:b # Parse all points from X
-        k = kernelmatrix(kernel(gp), X[i], alg.Z)
+        k = kernelmatrix(k, X[i], Z)
         # d = find_nearest_center(X[i,:],alg.centers,kernel)[2]
-        if maximum(k) < alg.ρ_accept #If biggest correlation is smaller than threshold add point
-            alg.Z = push!(alg.Z, X[i])
+        if maximum(k) < Z.ρ_accept #If biggest correlation is smaller than threshold add point
+            alg.Z = push!(Z.Z, deepcopy(X[i]))
             alg.k += 1
         end
         while alg.k > alg.kmax ## If maximum number of points is reached, readapt the threshold
-            K = kernelmatrix(kernel(gp), alg.Z)
+            K = kernelmatrix(k, Z)
             m = maximum(K - Diagonal(K))
-            alg.ρ_remove = alg.η * m
-            remove_point!(alg, K, kernel(gp))
-            if alg.ρ_remove < alg.ρ_accept
-                alg.ρ_accept = alg.η * alg.ρ_remove
+            Z.ρ_remove = Z.η * m
+            remove_point!(Z, K, k)
+            if Z.ρ_remove < Z.ρ_accept
+                Z.ρ_accept = Z.η * Z.ρ_remove
             end
-            @info "ρ_accept reset to : $(alg.ρ_accept)"
+            @info "ρ_accept reset to : $(Z.ρ_accept)"
         end
     end
 end
 
-function remove_point!(alg::OIPS, K, kernel)
-    if alg.k > 10
-        overlapcount = (x -> count(x .> alg.ρ_remove)).(eachrow(K))
+function remove_point!(Z::OIPS, K::AbstractMatrix, kernel::Kernel)
+    if Z.k > Z.kmin
+        overlapcount = (x -> count(x .> Z.ρ_remove)).(eachrow(K))
         removable = SortedSet(findall(x -> x > 1, overlapcount))
         toremove = []
         c = 0
-        while !isempty(removable) && alg.k > 10
+        while !isempty(removable) && Z.k > 10
             i = sample(
                 collect(removable),
                 Weights(overlapcount[collect(removable)]),
             )
-            connected = findall(x -> x > alg.ρ_remove, K[i, :])
+            connected = findall(x -> x > Z.ρ_remove, K[i, :])
             overlapcount[connected] .-= 1
             outofloop = filter(x -> overlapcount[x] <= 1, connected)
             for j in outofloop
@@ -115,9 +118,9 @@ function remove_point!(alg::OIPS, K, kernel)
             if issubset(i, removable)
                 delete!(removable, i)
             end
-            alg.k -= 1
+            Z.k -= 1
         end
-        alg.Z = alg.Z[setdiff(1:alg.k, toremove)]
-        alg.k = size(alg.Z, 1)
+        Z.Z = Z.Z[setdiff(1:Z.k, toremove)]
+        Z.k = size(Z.Z, 1)
     end
 end
