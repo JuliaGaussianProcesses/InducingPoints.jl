50addc01068d4733a9df750ed43ad00b133dc94e
Steffen-Mac <sridder1@mail.upb.de>
2021-12-21 14:34:03 -0800

    Add unigrid tests

---
+++ test/online/unigrid.jl
@@ -0,0 +1,10 @@
+@testset "UniGrid" begin
+    seed!(42)
+    N = 30
+    D = 3
+    nInd = 20
+    X = ColVecs(rand(D, N) * 10)
+    alg = UniGrid(nInd)
+    @test repr(alg) == "Uniform grid with side length $nInd."
+    test_Zalg(alg)
+end 
