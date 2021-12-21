dc9d00a87ee41fc5bd4ff25ebf8d9b55e00f3ca8
Steffen-Mac <sridder1@mail.upb.de>
2021-12-20 22:09:19 -0800

    Make things AbstractVectors

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -15,27 +15,27 @@ function test_Zalg(alg::InducingPoints.OnIPSA, N::Int=30, D::Int=3; kwargs...)
         for T in [Float64, Float32]
             X = [rand(T, D) * 10 for _ in 1:N]
             Z = inducingpoints(alg, X; arraytype=Vector{T}, kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
             X2 = [rand(T, D) * 10 for _ in 1:N]
             Z = updateZ!(Z, alg, X2; kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
             Z = updateZ(Z, alg, X2; kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
         end
     end
     @testset "Test ColVecs" begin
         for T in [Float64, Float32]
             X = ColVecs(rand(T, D, N) * 10)
             Z = inducingpoints(alg, X; arraytype=Vector{T}, kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
             X2 = ColVecs(rand(T, D, 2 * N) * 10)
             X3 = [rand(T, D) * 10 for _ in 1:N]
             Z = updateZ!(Z, alg, X2; kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
             Z = updateZ!(Z, alg, X3; kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
             Z = updateZ(Z, alg, X2; kwargs...)
-            @test eltype(Z) == Vector{T}
+            @test eltype(Z) <: AbstractVector{T}
         end
     end
 end
