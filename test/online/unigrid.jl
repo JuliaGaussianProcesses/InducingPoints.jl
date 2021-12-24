12fbba0fef04f3325e41292e3ae9309112966a2f
Steffen-Mac <steffen@robots.ox.ac.uk>
2021-12-24 15:55:38 -0800

    First round of tests

--- test/online/unigrid.jl
+++ test/online/unigrid.jl
@@ -7,4 +7,39 @@
     alg = UniGrid(nInd)
     @test repr(alg) == "Uniform grid with side length $nInd."
     test_Zalg(alg)
+
+    ### 1-dim grid
+    @testset "1-dim UniformGrid" begin
+        proditer = Iterators.product(1.:5.)
+        gridp = [only(collect(x)) for x in collect(proditer)]
+        ug = UniformGrid(proditer)
+
+        # getindex
+        @test ug[2] == gridp[2]
+        @test ug[2:4] == gridp[2:4]
+        @test ug[:] == gridp[:]
+
+        @test IndexStyle(ug) isa IndexLinear
+
+        # broadcast
+        @test norm.(ug) == norm.(gridp)
+    end
+    
+    ### >1-dim grid
+    @testset ">1-dim grid" begin
+        proditer = Iterators.product(1.:5., 6.:10.)
+        gridp = collect.(collect(proditer))[:]
+        ug = UniformGrid(proditer)
+
+        # getindex
+        @test ug[2] == gridp[2]
+        @test ug[2:4] == gridp[2:4]
+        @test ug[:] == gridp[:]
+
+        @test IndexStyle(ug) isa IndexLinear
+
+        # broadcast
+        @test norm.(ug) == norm.(gridp)
+    end
+
 end
