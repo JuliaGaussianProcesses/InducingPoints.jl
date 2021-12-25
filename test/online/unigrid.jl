2af1065abbb7c8d8828a2b011781da5c1323efdc
Steffen-Mac <steffen@robots.ox.ac.uk>
2021-12-24 18:59:30 -0800

    Last tests (hopefully)

--- test/online/unigrid.jl
+++ test/online/unigrid.jl
@@ -8,37 +8,45 @@
     @test repr(alg) == "Uniform grid with side length $nInd."
     test_Zalg(alg)
 
+    function testUG(proditer, gridp, ug)
+        # getindex
+        @test ug[2] == gridp[2]
+        @test ug[2:4] == gridp[2:4]
+        @test ug[:] == gridp[:]
+
+        @test IndexStyle(ug) isa IndexLinear
+
+        # broadcast
+        @test sum.(ug) == sum.(gridp)
+
+        # size
+        @test length(ug) == length(proditer)
+        @test size(ug) == (length(proditer),)
+
+        @test [(i,a) for (i,a) in enumerate(ug)] == [(i,a) for (i,a) in enumerate(gridp)]
+    end
+
     ### 1-dim grid
     @testset "1-dim UniformGrid" begin
         proditer = Iterators.product(1.0:5.0)
         gridp = [only(collect(x)) for x in collect(proditer)]
         ug = UniformGrid(proditer)
 
-        # getindex
-        @test ug[2] == gridp[2]
-        @test ug[2:4] == gridp[2:4]
-        @test ug[:] == gridp[:]
+        testUG(proditer, gridp, ug)
 
-        @test IndexStyle(ug) isa IndexLinear
-
-        # broadcast
-        @test sum.(ug) == sum.(gridp)
+        @test repr("text/plain", ug) == "(5,) uniform grid with edges\n[1.0, 5.0]\n"
+        @test repr(ug) == "(5,) uniform grid"
     end
 
     ### >1-dim grid
-    @testset ">1-dim grid" begin
+    @testset " >1-dim grid" begin
         proditer = Iterators.product(1.0:5.0, 6.0:10.0)
         gridp = collect.(collect(proditer))[:]
         ug = UniformGrid(proditer)
 
-        # getindex
-        @test ug[2] == gridp[2]
-        @test ug[2:4] == gridp[2:4]
-        @test ug[:] == gridp[:]
+        testUG(proditer, gridp, ug)
 
-        @test IndexStyle(ug) isa IndexLinear
-
-        # broadcast
-        @test sum.(ug) == sum.(gridp)
+        @test repr("text/plain", ug) == "(5, 5) uniform grid with edges\n[1.0, 5.0]\n[6.0, 10.0]\n"
+        @test repr(ug) == "(5, 5) uniform grid"
     end
 end
