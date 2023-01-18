9548c465a41af9fce5ae4f6c216afd44524b50ee
theogf <theo.galyfajou@gmail.com>
2023-01-19 00:04:04 +0100

    oh no

--- test/offline/kdpp.jl
+++ test/offline/kdpp.jl
@@ -6,10 +6,10 @@
     kernel = SqExponentialKernel()
     X = ColVecs(rand(D, N))
     alg = kDPP(nInd)
-    @test repr(alg) == "k-DPP selection of inducing points"
+    @test repr(alg) == "k-DPP selection of $(nInd) inducing points"
     Z = inducingpoints(alg, X; kernel)
     @test length(Z) == nInd
-    @test_throws ArgumentError kDPP(-1, kernel)
+    @test_throws ArgumentError kDPP(-1)
     @test_throws ErrorException inducingpoints(kDPP(100), X; kernel)
-    test_Zalg(kDPP(nInd, kernel), N)
+    test_Zalg(kDPP(nInd), N; kernel)
 end
